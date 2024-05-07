42b40209246b21e0ee62dd4006cd1d8246463916
sebastien544 <59696100+sebastien544@users.noreply.github.com>
2024-05-07 17:04:16 +0700

    Update search-result.js

--- search-result.js
+++ search-result.js
@@ -141,6 +141,7 @@ document.addEventListener("DOMContentLoaded", async function(){
     let results = await searchAll(query, page);
     if (results.length == 0) {
       results = await suggest(query);
+      document.getElementById('suggestions').innerText('Voici quelques suggestions');
     }
     const baseUrl = window.location.origin.includes('webflow.io') 
     ? 'https://ordotype.webflow.io' 
