fd379bae9df8875af82787855a3dc5788374e2e0
sebastien544 <59696100+sebastien544@users.noreply.github.com>
2024-10-10 13:41:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-
+searchBar?.addEventListener("keydown", (e) => {
+ if (e.key === 'Enter') {
+      e.preventDefault(); // Empêche l'action par défaut pour la touche Entrée
+  }
+  keyDownEvent(e);
+});
