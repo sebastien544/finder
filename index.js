fa39d4265c54b41b9db357fc906db3b73670cb85
sebastien544 <59696100+sebastien544@users.noreply.github.com>
2024-10-10 13:52:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,31 @@ searchBar?.addEventListener("keydown", (e) => {
   }
   keyDownEvent(e);
 });
+
+
+function keyDownEvent(e) {
+  var x = document.getElementById("search-results") || document.querySelector(`div[data-w-tab="${activeTab}"] div.search-result-body`);
+  if (x) x = x.getElementsByTagName("a");
+  if (e.keyCode == 40) {
+    currentFocus++;
+    /*and and make the current item more visible:*/
+    addActive(x);
+  } else if (e.keyCode == 38) {
+    //up
+    /*If the arrow UP key is pressed,
+    decrease the currentFocus variable:*/
+    currentFocus--;
+    /*and and make the current item more visible:*/
+    addActive(x);
+  } else if (e.keyCode == 13) {
+    /*If the ENTER key is pressed, prevent the form from being submitted,*/
+    e.preventDefault();
+    if (currentFocus > -1) {
+      /*and simulate a click on the "active" item:*/
+      if (x) x[currentFocus].click();
+    } else {
+        const query = e.currentTarget.value.trim();
+        window.location.href = `${baseUrl}/search-result?query=${query}&page=1`;
+    }
+  }
+}
