bb69496f23f4539a14d30a2cb845d7f88405fbad
sebastien544 <59696100+sebastien544@users.noreply.github.com>
2024-10-11 16:49:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+searchBarMain?.addEventListener('blur', () => {
+   var query = searchBar.value.trim()
 
+   setTimeout(function() {
+      query.length > 0 && updateQueryCount(query, true, false);
+  }, 2000)
+});
+
+searchBarMain?.addEventListener("focus", async () => {
+   const query = searchBar.value.trim();
+
+   if (query) {
+    let filterStored = getItemWithExpiration('filterTemp') || "";
+    const results = await search(query, filterStored);
+    if(searchBarMain){
+      handleSendResultsToGA("search-bar-focus");
+    }else{
+      handleSendResultsToGA("search-bar-nav-focus");
+    }
+    displayResults(results, searchBarMain);
+   }
+});
