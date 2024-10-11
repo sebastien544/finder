8adc6aa55ff2130d15ae75afb014652a629f868c
sebastien544 <59696100+sebastien544@users.noreply.github.com>
2024-10-11 18:11:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -21,39 +21,50 @@ searchBarMain?.addEventListener("focus", async () => {
    }
 });
 
-async function suggest(query) {
-   try {
-     const response = await axios.post(
-       `${ES_URL}/_search`,
-//       //"https://ordotype-finder.es.eu-west-3.aws.elastic-cloud.com/ordotype-index-staging-2024-05-23/_search",
-       {
-         suggest: {
-           suggestion: {
-             prefix: query,
-             completion: {
-               field: "Slug",
-               fuzzy: {
-                 fuzziness: "2",
-               },
-             },
-           },
-         },
-       },
-       {
-         headers: {
-           "Content-Type": "application/json",
-           Authorization:
-             "ApiKey bFk2VGs0Y0JHcFJXRm1EZENyaGU6R0xpOHdPUENUSXlxS3NvMGhna3JTUQ==",
-         },       }
-     );
+searchBar?.removeEventListener("keydown", (e) => {
+  keyDownEvent(e);
+});
 
-     return response.data.suggest.suggestion[0].options.map((option) => ({
-       Name: option._source.Name,
-      Slug: option._source.Slug,
-       wordingLogo: hit._source.Wording_Logo,
-       Img: option._source.Logo_for_finder_URL,
-     }));
-   } catch (error) {
-     console.error(error);
-  }
+searchBar?.addEventListener("keydown", (e) => {
+  if (e.key === 'Enter') {
+     e.preventDefault(); // Empêche l'action par défaut pour la touche Entrée
  }
+  keyDownEvent(e);
+});
+
+//async function suggest(query) {
+//   try {
+//     const response = await axios.post(
+//       `${ES_URL}/_search`,
+// //       //"https://ordotype-finder.es.eu-west-3.aws.elastic-cloud.com/ordotype-index-staging-2024-05-23/_search",
+//       {
+//         suggest: {
+//           suggestion: {
+//             prefix: query,
+//             completion: {
+//               field: "Slug",
+//               fuzzy: {
+//                 fuzziness: "2",
+//               },
+//             },
+//           },
+//         },
+//       },
+//       {
+//         headers: {
+//           "Content-Type": "application/json",
+//           Authorization:
+//             "ApiKey bFk2VGs0Y0JHcFJXRm1EZENyaGU6R0xpOHdPUENUSXlxS3NvMGhna3JTUQ==",
+//         },       }
+//     );
+
+//     return response.data.suggest.suggestion[0].options.map((option) => ({
+//       Name: option._source.Name,
+//      Slug: option._source.Slug,
+//       wordingLogo: hit._source.Wording_Logo,
+//       Img: option._source.Logo_for_finder_URL,
+//     }));
+//   } catch (error) {
+//     console.error(error);
+//  }
+// }
